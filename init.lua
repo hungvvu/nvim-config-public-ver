36b4a4e546325d7058c2dfc86a2b79e73c690847
Hung Vu <hung.h.vu@aalto.fi>
2024-04-02 10:12:58 +0300

    feat: Modularised the plugins and config into individual files

--- init.lua
+++ init.lua
@@ -1,46 +1,23 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-vim.g.mapleader = " "
+require("keymaps")
+
 
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
-local plugins = {
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-  {
-    'nvim-telescope/telescope.nvim', tag = '0.1.6',
-      dependencies = { 'nvim-lua/plenary.nvim' }
-  },
-  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}
-}
-local opts = {}
 
-require("lazy").setup(plugins, opts)
-
-require("plugins.catppuccino")
-
-local builtin = require("telescope.builtin")
-vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+-- Load all the plugins with lazy.
+require("lazy").setup("plugins")
 
 
 
-local config = require("nvim-treesitter.configs")
-config.setup({
-  ensure_installed = {"lua", "python", "yaml"},
-  highlight = { enable = true },
-  indent = { enable = true }
-})
