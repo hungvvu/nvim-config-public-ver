13c4ab9455da2883804d87747e6ea4dfb909e6da
Hung Vu <hung.h.vu@aalto.fi>
2024-04-04 19:47:05 +0300

    feat: Added keymap for entering terminal mode

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -5,3 +5,4 @@ vim.cmd("set shiftwidth=4")
 vim.g.mapleader = " "
 
 vim.api.nvim_set_keymap('t', '<Esc>', '<C-\\><C-n>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<A-t>', ':terminal<CR>', {noremap = true, silent = true})
