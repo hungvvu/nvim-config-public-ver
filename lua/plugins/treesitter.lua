951df04998f233a7b24686fc387ddc672ef4ce68
Hung Vu <hung.h.vu@aalto.fi>
2024-04-11 22:40:27 +0300

    feat: Adding some parser to treesitter to fix compatibility problems Previously there were some problems with displaying the Vim document caused by the parser. Adding vimdoc along with other parsers to ensured_install should fixes that.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,9 +4,9 @@ return {
     config = function()
         local config = require("nvim-treesitter.configs")
         config.setup({
-            ensure_installed = {"lua", "python", "yaml"},
             highlight = { enable = true },
-            indent = { enable = true }
+            indent = { enable = true },
+            ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'yaml', 'rust', 'typescript', 'vimdoc', 'cmake' }
         })
         end
 }
