8fa93958484c819f83714e2ac77742426c31097f
Hung Vu <hung.h.vu@aalto.fi>
2024-04-14 20:05:59 +0300

    fix: Overlapping keymap problem with Harpoon and Nvim-tmux-navigator They both used Ctrl and hjkl to navigate. Changed Harpoon to use another mapping instead.

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -8,43 +8,43 @@ return {
         vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
         vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-        vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
-        vim.keymap.set("n", "<C-j>", function() harpoon:list():select(2) end)
-        vim.keymap.set("n", "<C-k>", function() harpoon:list():select(3) end)
-        vim.keymap.set("n", "<C-l>", function() harpoon:list():select(4) end)
-        vim.keymap.set("n", "<leader><C-h>", function() harpoon:list():replace_at(1) end)
-        vim.keymap.set("n", "<leader><C-j>", function() harpoon:list():replace_at(2) end)
-        vim.keymap.set("n", "<leader><C-k>", function() harpoon:list():replace_at(3) end)
-        vim.keymap.set("n", "<leader><C-l>", function() harpoon:list():replace_at(4) end)
+        vim.keymap.set("n", "<leader>hh", function() harpoon:list():select(1) end)
+        vim.keymap.set("n", "<leader>hj", function() harpoon:list():select(2) end)
+        vim.keymap.set("n", "<leader>hk", function() harpoon:list():select(3) end)
+        vim.keymap.set("n", "<leader>hl", function() harpoon:list():select(4) end)
+        vim.keymap.set("n", "<leader>hH", function() harpoon:list():replace_at(1) end)
+        vim.keymap.set("n", "<leader>hJ", function() harpoon:list():replace_at(2) end)
+        vim.keymap.set("n", "<leader>hK", function() harpoon:list():replace_at(3) end)
+        vim.keymap.set("n", "<leader>hL", function() harpoon:list():replace_at(4) end)
 
         -- Toggle previous & next buffers stored within Harpoon list
         -- vim.keymap.set("n", "<leader>hn", function() harpoon:list():prev() end, {noremap = true})
         -- vim.keymap.set("n", "<leader>hp", function() harpoon:list():next() end, {noremap = true})
 
-
-        -- Telescope configurations
-        -- local conf = require("telescope.config").values
-        -- local function toggle_telescope(harpoon_files)
-        --     local file_paths = {}
-        --     for _, item in ipairs(harpoon_files.items) do
-        --         table.insert(file_paths, item.value)
-        --     end
-
-        --     require("telescope.pickers").new({}, {
-        --         prompt_title = "Harpoon",
-        --         finder = require("telescope.finders").new_table({
-        --             results = file_paths,
-        --         }),
-        --         previewer = conf.file_previewer({}),
-        --         sorter = conf.generic_sorter({}),
-        --     }):find()
-        -- end
-
-        -- vim.keymap.set("n", "<C-e>", function() toggle_telescope(harpoon:list()) end,
-        --     { desc = "Open harpoon window" })
-        -- vim.keymap.set({'n' }, '<C-d>', delete_harpoon_mark, {noremap = true, desc = "Delete Harpoon mark"})
-        -- vim.keymap.set({'n'}, '<C-p>', move_mark_up, {noremap = true, desc = "Move Harpoon mark up"})
-        -- vim.keymap.set({'n'}, '<C-n>', move_mark_down, {noremap = true, desc = "Move Harpoon mark down"})
+-- 
+--         -- Telescope configurations
+--         -- local conf = require("telescope.config").values
+--         -- local function toggle_telescope(harpoon_files)
+--         --     local file_paths = {}
+--         --     for _, item in ipairs(harpoon_files.items) do
+--         --         table.insert(file_paths, item.value)
+--         --     end
+-- 
+--         --     require("telescope.pickers").new({}, {
+--         --         prompt_title = "Harpoon",
+--         --         finder = require("telescope.finders").new_table({
+--         --             results = file_paths,
+--         --         }),
+--         --         previewer = conf.file_previewer({}),
+--         --         sorter = conf.generic_sorter({}),
+--         --     }):find()
+--         -- end
+-- 
+--         -- vim.keymap.set("n", "<C-e>", function() toggle_telescope(harpoon:list()) end,
+--         --     { desc = "Open harpoon window" })
+--         -- vim.keymap.set({'n' }, '<C-d>', delete_harpoon_mark, {noremap = true, desc = "Delete Harpoon mark"})
+--         -- vim.keymap.set({'n'}, '<C-p>', move_mark_up, {noremap = true, desc = "Move Harpoon mark up"})
+--         -- vim.keymap.set({'n'}, '<C-n>', move_mark_down, {noremap = true, desc = "Move Harpoon mark down"})
     end
 
 }
--- lua/plugins/nvim-tmux-navigator.lua
+++ lua/plugins/nvim-tmux-navigator.lua
@@ -1,10 +1,14 @@
 return {
   "alexghergh/nvim-tmux-navigation",
+  event = "VeryLazy",
   config = function()
-    require('nvim-tmux-navigation').setup({})
-    vim.keymap.set("n", "<C-y>", ":NvimTmuxNavigateLeft<CR>", { noremap = true })
-    vim.keymap.set("n", "<C-u>", ":NvimTmuxNavigateDown<CR>", { noremap = true })
-    vim.keymap.set("n", "<C-i>", ":NvimTmuxNavigateUp<CR>", { noremap = true })
-    vim.keymap.set("n", "<C-o>", ":NvimTmuxNavigateRight<CR>", { noremap = true })
+    local nvim_tmux_nav = require("nvim-tmux-navigation")
+        
+    vim.keymap.set('n', "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft, { noremap = true })
+    vim.keymap.set('n', "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown, { noremap = true })
+    vim.keymap.set('n', "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp, { noremap = true })
+    vim.keymap.set('n', "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight, { noremap = true })
+    vim.keymap.set('n', "<C-\\>", nvim_tmux_nav.NvimTmuxNavigateLastActive, { noremap = true })
+    vim.keymap.set('n', "<C-Space>", nvim_tmux_nav.NvimTmuxNavigateNext, { noremap = true })
   end,
 }
