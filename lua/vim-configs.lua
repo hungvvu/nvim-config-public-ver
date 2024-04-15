54e8512e2d9bd219e76101bceb107396080fdbca
Hung Vu <hung.h.vu@aalto.fi>
2024-04-15 08:45:28 +0300

    refactor: Move the tab configs to vim-configs.lua from keymaps.lua Also change the tab size because after a while I realised that 4 spaces is a bit much

--- lua/vim-configs.lua
+++ lua/vim-configs.lua
@@ -1,3 +1,9 @@
 vim.wo.relativenumber = true
 vim.wo.number = true
 vim.o.scrolloff = 999
+
+
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
