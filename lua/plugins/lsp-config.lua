2b130742c3b7fbd8d4d73b120ec460559c0fbf7a
Hung Vu <hung.h.vu@aalto.fi>
2024-04-16 07:02:20 +0300

    fix(LSP): replace pyre with pyright as the Python LSP server pyre's features are rather limited and only offer diagnostic.
    See: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#pyre

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,34 +1,42 @@
 return {
-    {
-        "williamboman/mason.nvim",
-        config = function()
-            require("mason").setup()
-        end
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        config = function()
-            require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "sourcery" }
-            })
-        end
-    },
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
-            local lspconfig = require("lspconfig")
-            lspconfig.lua_ls.setup({
-                pip = {
-                    upgrade_pip = true,
-                }
-            })
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "sourcery", "pyright", "biome" }
+      })
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({
+        pip = {
+          upgrade_pip = true,
+        }
+      })
 
-            vim.keymap.set('n', 'H', vim.lsp.buf.hover, {})
-            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-            vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
+      vim.keymap.set('n', 'H', vim.lsp.buf.hover, {})
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+      vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
 
-            lspconfig.pyre.setup({})
-        end
-    }
+      lspconfig.pyright.setup({
+        root_dir = lspconfig.util.root_pattern('.git');
+      })
+      
+      lspconfig.eslint.setup({
+        root_dir = lspconfig.util.root_pattern('.git');
+      })
+      -- lspconfig.biome.setup({
+      -- })
+    end
+  }
 
 }
