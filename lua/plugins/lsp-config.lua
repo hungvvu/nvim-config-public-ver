af4ffefb77d4a30a5763f9b5da4ba220a4a2aa12
Hung Vu <hung.h.vu@aalto.fi>
2024-04-16 07:51:57 +0300

    feat: Add auto-completion capabilities for LSPs - Add auto-completion capabilities for LSPs - Add LSP for Typescript and Javascript

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,31 +9,45 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "sourcery", "pyright", "biome" }
+        ensure_installed = { "lua_ls", "sourcery", "pyright", "biome", "tsserver", "html"  }
       })
     end
   },
   {
     "neovim/nvim-lspconfig",
     config = function()
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+      -- Set up 
       local lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup({
         pip = {
           upgrade_pip = true,
-        }
+        },
+        capabilities = capabilities,
+      })
+      
+      lspconfig.pyright.setup({
+        root_dir = lspconfig.util.root_pattern('.git'),
+        capabilities = capabilities,
+      })
+      
+      lspconfig.eslint.setup({
+        root_dir = lspconfig.util.root_pattern('.git'),
+        capabilities = capabilities,
+      })
+
+      lspconfig.tsserver.setup({
+        capabilities = capabilities
+      })
+      
+      lspconfig.html.setup({
+        capabilities = capabilities
       })
 
       vim.keymap.set('n', 'H', vim.lsp.buf.hover, {})
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
       vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
 
-      lspconfig.pyright.setup({
-        root_dir = lspconfig.util.root_pattern('.git');
-      })
-      
-      lspconfig.eslint.setup({
-        root_dir = lspconfig.util.root_pattern('.git');
-      })
       -- lspconfig.biome.setup({
       -- })
     end
