22734a4440e29fbedaa79acd2b2d9a4b55fedf08
Hung Vu <hung.h.vu@aalto.fi>
2024-05-01 18:34:10 +0300

    feat: Add mapping for using luasnip snippets with tabstops - Change mapping for confirm completion from Tab to Enter - Tab now jump between tabstops (, , ..., ) of a snippet

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -15,6 +15,7 @@ return {
     },
     config = function()
       local cmp = require('cmp')
+      local luasnip = require('luasnip')
       require("luasnip.loaders.from_vscode").lazy_load()
       cmp.setup {
         snippet = {
@@ -27,9 +28,40 @@ return {
           ['<C-p>'] = cmp.mapping.select_prev_item(),  -- Previouss completion menu
           ['<C-d>'] = cmp.mapping.scroll_docs(-4),     -- Scroll docs in completion menu
           ['<C-f>'] = cmp.mapping.scroll_docs(4),      -- Scroll docs in completion menu
-          ['<Tab>'] = cmp.mapping.confirm({ select = true }), -- Confirm selection
+          -- ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Confirm selection
           ['<C-]>'] = cmp.mapping.complete(),      -- Manually trigger completion (not really necessary because nvim-cmp will auto display completion on its own)
           ['<C-q>'] = cmp.mapping.abort(),    -- Close the completion menu
+          ['<CR>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              if luasnip.expandable() then
+                luasnip.expand()
+              else
+                cmp.confirm({
+                  select = true,
+                })
+              end
+            else
+              fallback()
+            end
+          end),
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.locally_jumpable(1) then
+              luasnip.jump(1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.locally_jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
         }),
         sources = cmp.config.sources({
           { name = 'nvim_lsp' },  -- Source: Neovim LSP
