31238f3301034ec658cd00c76f4754a89485448f
Hung Vu <hung.h.vu@aalto.fi>
2024-05-03 20:22:03 +0300

    feat: Added keymap for writing selection and whole file to ~/clipboard.txt This aims to be a workaround for issue #2. With the two added keymap one can copy the selection or the whole file into the ~/clipboard.txt file, which makes copying easier than it was previously.
    Close #2

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -18,4 +18,35 @@ vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
 
 
 -- File operations
+--- Save/Write current file
 vim.keymap.set('n', '<leader>wf', ':w<CR>')
+
+--- Save selection to a file
+function WriteSelectionToFile(file_path)
+  -- Expand the '~' to the home directory
+  if file_path:sub(1, 1) == '~' then
+    local home = os.getenv('HOME')
+    if home then
+      file_path = home .. file_path:sub(2) -- Replace '~' in file_path with the path to the home directory
+    end
+  end
+  
+  vim.cmd('normal! y') -- Yank the current visual selection
+  local selection = vim.fn.getreg('') -- Get the content of the unnamed register
+  local file, err = io.open(file_path, 'w') -- Open the file in append mode
+
+  
+  -- Write the selection to the file and close it
+  if not file then
+    print(string.format("Error opening file '%s': ", file_path), err)
+    return
+  end
+  file:write(selection)
+  file:close()
+end
+
+--- Save selection to '~/clipboard.txt'
+vim.keymap.set('v', '<leader>cs', '<cmd>lua WriteSelectionToFile("~/clipboard.txt")<CR>', {noremap = true})
+
+--- Save the whole file to 
+vim.keymap.set({'n', 'v'}, '<leader>cc', ':w! ~/clipboard.txt<CR>', {noremap = true})
