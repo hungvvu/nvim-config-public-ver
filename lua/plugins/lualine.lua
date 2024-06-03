0c61ad6d530f78211ceb93afca8aec3a79e1d57c
Hung Vu <hung.v.vu.cs@gmail.com>
2024-06-03 07:35:08 +0300

    feat: Make the background transparent
    Used catppuccin's built-in transparent background settings to make
    the background transparent. Also added a functiono to toggle between
    transparent and non-transparent background
    
    Close #7

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,18 +1,18 @@
 return {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = function()
-        require('lualine').setup({
-            options = {
-                theme = 'dracula'
-            },
-            sections = {
-                lualine_a = {
-                    {
-                        'buffers',
-                    }
-                }
-            }
-        })
-    end
+  'nvim-lualine/lualine.nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  config = function()
+    require('lualine').setup({
+      options = {
+        theme = 'catppuccin'
+      },
+      sections = {
+        lualine_a = {
+          {
+            'buffers',
+          }
+        }
+      }
+    })
+  end
 }
